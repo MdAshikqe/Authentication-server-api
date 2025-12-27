c1ffa784cb4e5f1a9b4121668ec59d0718e2fa9a
Ashik <asikhosen865575@gmail.com>
2025-12-27 14:36:03 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Authentication server port is running",
+  });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+
+const Port = 3000;
+
+app.listen(Port, () => {
+  console.log("Server is running port", Port);
+});
