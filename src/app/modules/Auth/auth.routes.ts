c438d96a138916eb1e4643f7d0b2190c5239da4f
Ashik <asikhosen865575@gmail.com>
2026-01-01 15:25:47 +0600

    change password setup

--- src/app/modules/Auth/auth.routes.ts
+++ src/app/modules/Auth/auth.routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { AuthControllers } from "./auth.controller";
+import auth from "../../middleware/auth";
+import { UserRole } from "../../../../generated/prisma/enums";
 
 const router = express.Router();
 
 router.post("/login", AuthControllers.login);
 router.post("/refress-token", AuthControllers.refressToken);
-router.post("/change-password", AuthControllers.changePassword);
+router.post(
+  "/change-password",
+  auth(UserRole.ADMIN, UserRole.CLIENT, UserRole.SUPER_ADMIN),
+  AuthControllers.changePassword
+);
 
 export const AuthRoutes = router;
